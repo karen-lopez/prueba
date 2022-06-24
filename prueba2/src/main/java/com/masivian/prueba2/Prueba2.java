2933d2c3b157941dd486db272d20f95ea8b8cfdc
Karen Lopez <karen.lopez@masiv.com>
2022-06-24 10:01:03 -0500

    add repository

---
+++ prueba2/src/main/java/com/masivian/prueba2/Prueba2.java
@@ -0,0 +1,24 @@
+package com.masivian.prueba2;
+
+/**
+ *
+ * @author slopez
+ */
+public class Prueba2 {
+
+    private String greeting;
+    
+    public Prueba2(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    
+}
